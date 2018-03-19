a9043cfc20d388ef7a2aef260d90413a130566f1
Greg Lucas <magellanarts@gmail.com>
2018-03-19 12:47:43 -0700

    keywords meta tag

--- dev/header.php
+++ dev/header.php
@@ -2,6 +2,7 @@
 <html></html>
 <head>
     <title><?php the_field('page_title'); ?></title>
+    <meta name="keywords" content="<?php the_field('keywords'); ?>">
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
     <?php wp_head(); ?>
 </head>
